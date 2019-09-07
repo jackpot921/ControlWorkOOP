d1b9d401914add4fbea8bdd737905a4bbfa5cc1c
kurilo <jackpotd3@gmail.com>
2019-09-07 12:22:22 +0300

    ControlWorkOOP

---
+++ ControlWorkOOP/Files.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlWorkOOP
+{
+    public abstract class Files
+    {
+        public string Name { get; internal set; }
+        public string Extention { get; internal set; }
+        public string Size { get; internal set; }
+
+        public virtual void Parse(string input)
+        {
+            string[] splitInfo = input.Split(new char[] { '.', '(', ')', ';' }, StringSplitOptions.RemoveEmptyEntries);
+            Name = splitInfo[0];
+            Extention = splitInfo[1];
+            Size = splitInfo[2];
+        }
+    }
+
+    public class TextFiles : Files
+    {
+        public string Content { get; internal set; }
+
+        public override void Parse(string input)
+        {
+            string[] splitInfo = input.Split(new char[] { '.', '(', ')', ';' }, StringSplitOptions.RemoveEmptyEntries);
+            base.Parse(input);
+            Content = splitInfo[3];
+        }
+
+        public override string ToString()
+        {
+            return "Text:" + "\n\r" + 
+                "\t" + $"{Name}.{Extention}" + "\n\r" +
+                "\t\t" + $"Extention: {Extention}" + "\n\r" +
+                "\t\t" + $"Size: {Size}" + "\n\r" +
+                "\t\t" + $"Content: {Content}";
+        }
+    }
+
+    public class ImageFiles : Files
+    {
+        public string Resolution { get; internal set; }
+
+        public override void Parse(string input)
+        {
+            string[] splitInfo = input.Split(new char[] { '.', '(', ')', ';' }, StringSplitOptions.RemoveEmptyEntries);
+            base.Parse(input);
+            Resolution = splitInfo[3];
+        }
+
+        public override string ToString()
+        {
+            return "Images:" + "\n\r" +
+                "\t" + $"{Name}.{Extention}" + "\n\r" +
+                "\t\t" + $"Extention: {Extention}" +  "\n\r" + 
+                "\t\t" + $"Size: {Size}" + "\n\r" +
+                "\t\t" + $"Resolution: {Resolution}";
+        }
+    }
+
+    public class MovieFiles : ImageFiles
+    {
+        public string Length { get; internal set; }
+
+        public override void Parse(string input)
+        {
+            string[] splitInfo = input.Split(new char[] { '.', '(', ')', ';' }, StringSplitOptions.RemoveEmptyEntries);
+            base.Parse(input);
+            Resolution = splitInfo[3];
+            Length = splitInfo[4];
+        }
+
+        public override string ToString()
+        {
+            return "Movies:" + "\n\r" +
+                "\t" + $"{Name}.{Extention}" + "\n\r" +
+                "\t\t" + $"Extention: {Extention}" + "\n\r" +
+                "\t\t" + $"Size: {Size}" + "\n\r" +
+                "\t\t" + $"Resolution: {Resolution}" + "\n\r" +
+                "\t\t" + $"Length: {Length}";
+        }
+    }
+    public class OtherFiles : Files
+    {
+        public string Data { get; private set; }
+
+        public override void Parse(string input)
+        {
+            string[] splitInfo = input.Split(new char[] { '.', '(', ')', ';' }, StringSplitOptions.RemoveEmptyEntries);
+            base.Parse(input);
+            Data = splitInfo[3];
+        }
+        public override string ToString()
+        {
+            return "Other:" + "\n\r" +
+                "\t" + $"{Name}.{Extention}" + "\n\r" +
+                "\t\t" + $"Extention: {Extention}" + "\n\r" +
+                "\t\t" + $"Size: {Size}" + "\n\r" +
+                "\t\t" + $"Data: {Data}";
+        }
+    }
+}
--- ControlWorkOOP/Program.cs
+++ ControlWorkOOP/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CollectionsLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,57 @@ namespace ControlWorkOOP
 {
     class Program
     {
+        /*string text = @"Text: file.txt(6B); Some string content
+Image: img.bmp(19MB); 1920х1080
+Text:data.txt(12B); Another string
+Text:data1.txt(7B); Yet another string
+Movie:logan.2017.mkv(19GB); 1920х1080; 2h12m";*/
+
         static void Main(string[] args)
         {
+            string text = @"Text: file.txt(6B); Some string content
+Image: img.bmp(19MB); 1920x1080
+Text:data.txt(12B); Another string
+Text:data1.txt(7B); Yet another string
+Movie:logan.2017.mkv(19GB); 1920x1080; 2h12m
+VSFile:program.cs(12B); some program";
+            
+            MyList<Files> fileList = new MyList<Files>();
+            string[] files = text.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                string[] split = files[i].Split(':');
+                string fileType = split[0].ToLower();
+                switch (fileType)
+                {
+                    case "text":
+                        TextFiles textFile = new TextFiles();
+                        textFile.Parse(split[1]);
+                        fileList.Add(textFile);
+                        break;
+                    case "image":
+                        ImageFiles imageFile = new ImageFiles();
+                        imageFile.Parse(split[1]);
+                        fileList.Add(imageFile);
+                        break;
+                    case "movie":
+                        MovieFiles movieFile = new MovieFiles();
+                        movieFile.Parse(split[1]);
+                        fileList.Add(movieFile);
+                        break;
+                    default:
+                        OtherFiles otherFile = new OtherFiles();
+                        otherFile.Parse(split[1]);
+                        fileList.Add(otherFile);
+                        break;
+                }
+            }
+            for (int i = 0; i < fileList.Count; i++)
+            {
+                Console.WriteLine(fileList[i].ToString());
+            }
+            Console.ReadKey();
         }
     }
 }
